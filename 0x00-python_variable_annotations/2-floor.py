13646e4934bf73112dcff6906b045587dd92641b
jobnovice <jobkebede47@gmail.com>
2024-10-11 02:59:46 +0300

    new one

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""yet another basic annotation example"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float number"""
+    return (math.floor(n))
