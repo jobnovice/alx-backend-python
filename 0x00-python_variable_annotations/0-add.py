4a0c83ae41d4ba0b70f79a0c66ddaf38a8bbf647
jobnovice <jobkebede47@gmail.com>
2024-10-11 02:08:11 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""simple function defined to try variable annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """accpets two floats and returns their sum
+    Args:
+        a: a float
+        b: another float
+    returns: the sum of the two numbers which is a float
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""another anntation example script"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """accpets two strings as an argument
+       Args:
+            str1: string to be concatenated
+            str2: another string to be concatatenated
+       returns: A string
+    """
+    return str1 + str2
