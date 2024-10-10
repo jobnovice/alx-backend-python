4ba084d1fe242c5ba80943047502757021105fe6
jobnovice <jobkebede47@gmail.com>
2024-10-11 02:17:26 +0300

    maturin

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """simple function defined to try variable annotation"""
 
 
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """another anntation example script"""
 
 
