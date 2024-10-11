b0e99eacf4e2f6a7fc9af806d7176c197bb5a9cf
jobnovice <jobkebede47@gmail.com>
2024-10-12 00:04:13 +0300

    pyvalidated

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """new module imitated from variable annotaion"""
 
-
 from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     """accpets lists of float and returns their sum
         Args:
