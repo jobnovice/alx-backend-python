28a142f705f2cdaf1d86c2d531359ca82d89667c
jobnovice <jobkebede47@gmail.com>
2024-10-12 00:41:29 +0300

    driven interpyschicly

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""annotation example number seven depicted"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns the string alongside with square of whatever was sent for
+    us"""
+    to_tup: Tuple[str, float] = (k, v*v)
