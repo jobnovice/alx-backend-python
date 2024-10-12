5275d42a6d53bf70c4ea27697405c7b0062261a6
jobnovice <jobkebede47@gmail.com>
2024-10-12 16:55:06 +0300

    cut to si 6 - Xiao 9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """complex type in functions implemenated in here"""
 
-from collections.abc import Callable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
