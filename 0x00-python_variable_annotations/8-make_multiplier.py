7dd431cbfab5885367f0aece6241538a8ede5042
jobnovice <jobkebede47@gmail.com>
2024-10-12 16:51:15 +0300

    jungian

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""complex type in functions implemenated in here"""
+
+from collections.abc import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """wrote some annotations for the callable  function that we defined"""
+    def multiply(value: float) -> float:
+        """multiplies the float with the multiplier"""
+        return value * multiplier
+    return multiply
