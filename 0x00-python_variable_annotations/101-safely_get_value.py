de4370a1b2f1067fb4029e6aff87c03d3d7efb4b
jobnovice <jobkebede47@gmail.com>
2024-10-14 02:12:51 +0300

    popping tires

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """TypeVar is know going to be immplemenated"""
-
-from typing import TypeVar, Dict
+from typing import Mapping, Any, Optional, TypeVar, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[str, T], key: str,
-                     default: Optional[T] = None) -> Optional[T]:
-    """function annotated using TypeVar"""
-    if key in dct:
-        return dct[key]
-    else:
-        return default
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """Return dct[key] if key exists, else default."""
+    return dct[key] if key in dct else deault
