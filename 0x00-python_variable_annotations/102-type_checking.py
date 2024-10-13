74f65c1e6bdea444b33b3bd246c378cf2a3a66f2
jobnovice <jobkebede47@gmail.com>
2024-10-14 02:33:26 +0300

    yep

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Use mypy to validate the following piece of code and apply
+any necessary changes."""
+
+from typing import Tuple, Any, List
+
+
+def zoom_array(lst: Any, factor: int = 2) -> List:
+    """used mypy to validate any issues"""
+    zoomed_in: Any = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
