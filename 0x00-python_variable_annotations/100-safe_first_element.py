4299a0c3af9b189c153e18cab54d7c041fd387e9
jobnovice <jobkebede47@gmail.com>
2024-10-13 22:55:09 +0300

    5/10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ from typing import Sequence, Union, Any, Iterable
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """annotated the function to accept any sequence"""
     if lst:
         return lst[0]
     else:
