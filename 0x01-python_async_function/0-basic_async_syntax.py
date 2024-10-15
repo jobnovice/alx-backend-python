c2a8cdc35ff5bd87ff7c23edf8be2dcbcee8f46e
jobnovice <jobkebede47@gmail.com>
2024-10-16 02:23:50 +0300

    ele

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 import time
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """minimalstic implementaion of concurrency"""
     start_time = time.time()
     await asyncio.sleep(random.uniform(0, max_delay))
