1c952b082a0e6ad7e8a053588877f9a1e77bbbf6
jobnovice <jobkebede47@gmail.com>
2024-10-16 02:22:14 +0300

    this could be real

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""0-basic_async_syntax.py the basics of Async"""
+import asyncio
+import random
+import time
+
+
+async def wait_random(max_delay=10):
+    """minimalstic implementaion of concurrency"""
+    start_time = time.time()
+    await asyncio.sleep(random.uniform(0, max_delay))
+    end_time = time.time()
+    duration = end_time - start_time
+    return duration
