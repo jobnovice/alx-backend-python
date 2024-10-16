c4543dacf09061469616b8a3dd2710b674d96b49
jobnovice <jobkebede47@gmail.com>
2024-10-17 02:27:30 +0300

    what about this one

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Asynchronus comprehension defined"""
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[int, None]:
+async def async_generator() -> Generator[int, None, None]:
     """Async Generator that generates random number b/n 1and10"""
     for _ in range(10):
         await asyncio.sleep(1)
