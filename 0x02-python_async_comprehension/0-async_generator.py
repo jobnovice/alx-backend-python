fc4808f786fdc05fa5df1488122e097f81bae7fe
jobnovice <jobkebede47@gmail.com>
2024-10-17 11:35:23 +0300

    ????

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> Generator[float, None, None]
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator that generates random number b/n 1and10"""
     for _ in range(10):
         await asyncio.sleep(1)
