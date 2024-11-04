513a5eb17c40388b3905abb6804d86b9099f3366
jobnovice <jobkebede47@gmail.com>
2024-11-04 21:46:44 +0300

    ??

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python3
-"""unittest and parametrization"""
-from utils import access_nested_map
+"""
+Test for access_nested_map function
+"""
 import unittest
+from utils import access_nested_map, get_json, memoize
 from typing import Mapping, Sequence, Any
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """testing the access_nested_map"""
+    """
+    Tests the access_nested_map function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access(self, nested_map: Mapping, path: Sequence,
-                    expected: int) -> None:
-        """testing the access_nested_map with a bunch of outputs"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
+        """
+        Test the access_nested_map method.
+        Args:
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
+        """
+        response = access_nested_map(nested_map, path)
+        self.assertEqual(response, expected)
 
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
-    def test_exception(self, nested_map: Mapping,
-                       path: Sequence) -> None:
-        """raises an exception"""
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """
+        Test the access_nested_map method raises an error when expected to
+        Args:
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
+        """
         with self.assertRaises(Exception):
             access_nested_map(nested_map, path)
