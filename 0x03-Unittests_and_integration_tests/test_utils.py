1cfa32f624bd9146a773ee6b1bcc801c9ea8b15c
jobnovice <jobkebede47@gmail.com>
2024-11-04 21:43:41 +0300

    Orginally

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """unittest and parametrization"""
 from utils import access_nested_map
 import unittest
+from typing import Mapping, Sequence, Any
 from parameterized import parameterized
 
 
@@ -12,10 +13,17 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access(self, nested_map, path, expected):
+    def test_access(self, nested_map: Mapping, path: Sequence,
+                    expected: int) -> None:
         """testing the access_nested_map with a bunch of outputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-
-if __name__ == "__main__":
-    unittest.main()
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_exception(self, nested_map: Mapping,
+                       path: Sequence) -> None:
+        """raises an exception"""
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
